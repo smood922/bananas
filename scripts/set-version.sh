9255ae77844fc97ef8660aa33fd1a48076c32b2d
Marco Kellershoff <marco@kellershoff.net>
2024-10-26 23:28:24 +0200

    feat(ci): add ci

---
+++ scripts/set-version.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+if [ -z "$VERSION" ]; then echo "Error: VERSION is not set"; exit 1; fi
+
+update_package_json_version() {
+  local tmp
+  tmp=$(mktemp)
+  jq --arg v "$VERSION" '.version = $v' package.json > "$tmp" && mv "$tmp" package.json
+}
+
+update_package_json_version
