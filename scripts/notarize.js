882d16bf97da6f05b6eb1d6e9a5d76e4e26f2520
Marco Kellershoff <marco@kellershoff.net>
2024-12-01 12:49:13 +0100

    feat(ux): add notarize webhook
    Webhook will post notarize status in discord

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -14,6 +14,7 @@ exports.default = async function notarizing(context) {
     teamId: process.env.APPLE_TEAM_ID,
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+    webhook: process.env.APPLE_NOTARIZATION_WEBHOOK_URL
   })
 }
